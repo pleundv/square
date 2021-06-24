4e8df3ec0329a1ae3512237443d97afaf0cd07f3
pleundv <84378584+pleundv@users.noreply.github.com>
2021-06-24 10:47:07 +0200

    Add files via upload

---
+++ square.js
@@ -0,0 +1,22 @@
+function squareNumber(no1) {
+    const squared = no1 ** 2;
+    return no1 ** 2;
+
+};
+
+let squared = squareNumber(5);
+console.log(squared);
+
+const squaresNumber = function (no2) {
+    const squared1 = no2 ** 2;
+    return no2 ** 2;
+};
+
+console.log(squaresNumber(4));
+
+
+const squarenNumber = (no3) => {
+    return no3 ** 2;
+};
+
+console.log(squarenNumber(7));
